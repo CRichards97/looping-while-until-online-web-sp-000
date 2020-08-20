60f6078a47f29f1a81a240be8da6193eda50cb2e
Clint Richards <richardsclint17@gmail.com>
2020-08-20 18:20:58 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -6,7 +6,4 @@ while levitation_force < 10
   levitation_force += 1
   puts "Wingardium Leviosa#{levitation_force}."
 end
-until using_until == 10
-    puts "Wingardium Leviosa#{levitation_force}."
-end
 end
