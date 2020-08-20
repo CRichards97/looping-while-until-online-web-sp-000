6556de1dc906f6f6de752f2ca6f0d7196dfa5561
Clint Richards <richardsclint17@gmail.com>
2020-08-20 17:54:23 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -4,11 +4,11 @@ def using_while
 
 while using_while < 10
   levitation_force += 1
-  puts "Wingardium Leviosa#{levitation_force}."
+    puts "Wingardium Leviosa#{levitation_force}."
 end
 # => nil (return value)
 until using_until == 10
   puts "Wingardium Leviosa#{levitation_force}."
-  levitation_force += 1
+    levitation_force += 1
 end
 end
